553aca46993aebe44c5c12ccfac33dcf4ba58ceb
Khoi Hoang <hello@koicha.dev>
2020-08-18 22:56:43 +0200

    Feature/GUI: :lipstick: Added News & Noteworthy headline section

--- template-parts/news_headliner.php
+++ template-parts/news_headliner.php
@@ -1,3 +1,6 @@
 <section class="main-grid-container__news_headliner">
+    <img src="<?php echo get_theme_file_uri() . './assets/img/polygon.svg' ?>" alt>
+    <div class="main-grid-container__news_headliner__box">
     <h1>News & Noteworthy</h1>
+    </div>
 </section>
