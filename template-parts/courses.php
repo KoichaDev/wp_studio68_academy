fe4856cabcccdcf43ebc15b2dd672d8c87b74aee
Khoi Hoang <hello@koicha.dev>
2020-08-18 22:23:29 +0200

    Feature/GUI: :lipstick: Added Card Body for courses
    - Currently it's a placeholder as a static card. 
    - The card body is also responsive

--- template-parts/courses.php
+++ template-parts/courses.php
@@ -1,3 +1,68 @@
 <section class="main-grid-container__courses">
-      Courses
+      <div class="container__courses__card">
+            <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__courses__card__card-body">
+                  <h1 class="container__courses__card__card-title">Confluence Course</h1>
+                  <p class="container__courses__card__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__courses__card__card-btn">Get Started »</a>
+            </div>
+      </div>
+
+      <div class="container__courses__card">
+            <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__courses__card__card-body">
+                  <h1 class="container__courses__card__card-title">Confluence Course</h1>
+                  <p class="container__courses__card__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__courses__card__card-btn">Get Started »</a>
+            </div>
+      </div>
+
+      <div class="container__courses__card">
+            <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__courses__card__card-body">
+                  <h1 class="container__courses__card__card-title">Confluence Course</h1>
+                  <p class="container__courses__card__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__courses__card__card-btn">Get Started »</a>
+            </div>
+      </div>
+
+      <div class="container__courses__card">
+            <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__courses__card__card-body">
+                  <h1 class="container__courses__card__card-title">Confluence Course</h1>
+                  <p class="container__courses__card__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__courses__card__card-btn">Get Started »</a>
+            </div>
+      </div>
+
+      <div class="container__courses__card">
+            <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__courses__card__card-body">
+                  <h1 class="container__courses__card__card-title">Confluence Course</h1>
+                  <p class="container__courses__card__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__courses__card__card-btn">Get Started »</a>
+            </div>
+      </div>
+
+      <div class="container__courses__card">
+            <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__courses__card__card-body">
+                  <h1 class="container__courses__card__card-title">Confluence Course</h1>
+                  <p class="container__courses__card__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__courses__card__card-btn">Get Started »</a>
+            </div>
+      </div>
+      
 </section>
