a5b5549c42585271fc7ac8478b1126163e023930
Khoi Hoang <hello@koicha.dev>
2020-08-18 21:26:39 +0200

    Feature: :sparkles: Added Google Font for Nunito Sans

--- header.php
+++ header.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
     <script src="https://kit.fontawesome.com/b3ce871dc0.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
     <title><?php echo get_bloginfo('name'); ?></title>
     
 </head>
