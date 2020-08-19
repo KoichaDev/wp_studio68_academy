62afeacff3dbb65177f2a9f9f2a15675549a9c3d
Khoi Hoang <hello@koicha.dev>
2020-08-19 10:44:44 +0200

    Feature/CSS: :lipstick: Added News & Noteworthy CSS Grid Section
    It is temporarily a placeholder. Next step is to style it and it and making it look more correct

--- template-parts/news.php
+++ template-parts/news.php
@@ -1,3 +1,7 @@
 <section class="main-grid-container__news">
-    Blog News
+    <div class="main-grid-container__news__top-article">top</div>
+    <div class="main-grid-container__news__middle-left-article">mid-left</div>
+    <div class="main-grid-container__news__middle-right-article">mid-right</div>
+    <div class="main-grid-container__news__bottom-left-article">bottom-left</div>
+    <div class="main-grid-container__news__bottom-right-article">bottom-right</div>
 </section>
