8af04cf85960a5ac11a112372ec6d76b0b4c5f47
Khoi Hoang <hello@koicha.dev>
2020-08-19 12:10:02 +0200

    Feature/GUI: :lipstick: Added Top static blog news & noteworthy section
    - The card is just a placeholder

--- template-parts/news.php
+++ template-parts/news.php
@@ -1,7 +1,19 @@
-<section class="main-grid-container__news">
-    <div class="main-grid-container__news__top-article">top</div>
-    <div class="main-grid-container__news__middle-left-article">mid-left</div>
-    <div class="main-grid-container__news__middle-right-article">mid-right</div>
-    <div class="main-grid-container__news__bottom-left-article">bottom-left</div>
-    <div class="main-grid-container__news__bottom-right-article">bottom-right</div>
-</section>
+<div class="main-grid-container__news">
+    <article class="main-grid-container__news__top-article">
+        <img src="http://source.unsplash.com/random/525x378" alt="" class="main-grid-container__news__top-article__image">
+        <div class="main-grid-container__news__top-article__card-body">
+            <span>Career Advice</span>
+            <h4>Career change advice from computer Turned to a Human Resourcer</h4>
+            <p>
+                <small>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto perspiciatis ut totam illum saepe id blanditiis laboriosam, dolores aut delectus facilis libero tempore molestias dicta nemo assumenda debitis consequuntur ab.    
+                </small>   
+            </p>
+            <a href="#">Read More »</a>
+        </div>
+    </article>
+    <article class="main-grid-container__news__middle-left-article">mid-left</article>
+    <article class="main-grid-container__news__middle-right-article">mid-right</article>
+    <article class="main-grid-container__news__bottom-left-article">bottom-left</article>
+    <article class="main-grid-container__news__bottom-right-article">bottom-right</article>
+</div>
