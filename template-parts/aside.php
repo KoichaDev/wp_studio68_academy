bb2cb330e709e9c9cb3ff1d68e3ee77e76eeea5d
Khoi Hoang <hello@koicha.dev>
2020-09-07 18:46:56 +0200

    Feature/GUI: :lipstick: Added a placeholder for sidebar

--- template-parts/aside.php
+++ template-parts/aside.php
@@ -1,1 +1,11 @@
- <aside class="main-grid-container__aside">Aside</aside>
+ <aside class="main-grid-container__aside">
+     <div class="aside-homepage-sticky">
+        <h1>Subjects</h1>
+        <ul>
+            <li>Web Development</li>
+            <li>Project Management</li>
+            <li>Design Course</li>
+            <li>Coding Course</li>
+        </ul>
+     </div>
+ </aside>
