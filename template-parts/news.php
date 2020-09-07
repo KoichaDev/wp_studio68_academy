3463803e011afc5616a8998fc61730893dac21d6
Khoi Hoang <hello@koicha.dev>
2020-09-07 20:31:37 +0200

    Add: :sparkles: Added dynamically post on the front page
    Admins can post "post" on the website for news section

--- template-parts/news.php
+++ template-parts/news.php
@@ -1,45 +1,22 @@
 <article class="main-grid-container__news">
-    <article class="container__news__card">
-         <img src="http://source.unsplash.com/random/288x236" alt>
-            <div class="container__news__card-body">
-                  <h1 class="container__news__card-title">Confluence Course</h1>
-                  <p class="container__news__card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                  </p>
-                  <a href="#" class="container__news__card-btn">Get Started »</a>
-            </div>
-    </article>
-
-    <article class="container__news__card">
-         <img src="http://source.unsplash.com/random/288x236" alt>
-            <div class="container__news__card-body">
-                  <h1 class="container__news__card-title">Confluence Course</h1>
-                  <p class="container__news__card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                  </p>
-                  <a href="#" class="container__news__card-btn">Get Started »</a>
-            </div>
-    </article>
-
-    <article class="container__news__card">
-         <img src="http://source.unsplash.com/random/288x236" alt>
-            <div class="container__news__card-body">
-                  <h1 class="container__news__card-title">Confluence Course</h1>
-                  <p class="container__news__card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                  </p>
-                  <a href="#" class="container__news__card-btn">Get Started »</a>
-            </div>
-    </article>
-
-    <article class="container__news__card">
-         <img src="http://source.unsplash.com/random/288x236" alt>
-            <div class="container__news__card-body">
-                  <h1 class="container__news__card-title">Confluence Course</h1>
-                  <p class="container__news__card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                  </p>
-                  <a href="#" class="container__news__card-btn">Get Started »</a>
-            </div>
-    </article>
+      <?php 
+      $newsArticle = new WP_Query([
+            'posts_per_page' => -1
+      ]); 
+      ?>
+      <?php while($newsArticle -> have_posts()) { 
+            $newsArticle -> the_post(); 
+      ?>
+      <article class="container__news__card">
+            <img src="http://source.unsplash.com/random/288x236" alt>
+                  <div class="container__news__card-body">
+                        <h1 class="container__news__card-title"><?php echo get_the_title(); ?></h1>
+                        <p class="container__news__card-text">
+                              <?php echo wp_trim_words( get_the_excerpt(), 18); ?></p>
+                        <a href="<?php the_permalink(); ?>" class="container__news__card-btn">Get Started »</a>
+                  </div>
+      </article>
+      <?php } 
+            wp_reset_postdata();
+      ?>
 </article>
