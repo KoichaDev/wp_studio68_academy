969adc73f569dc02cac9c5ed4f243481a46dc77b
Khoi Hoang <hello@koicha.dev>
2020-09-07 18:15:23 +0200

    Update/GUI: :lipstick: Changed the News section card as temporarily look
    - Will follow Figma Design later. First is to get it to work

--- template-parts/news.php
+++ template-parts/news.php
@@ -1,19 +1,45 @@
-<div class="main-grid-container__news">
-    <article class="main-grid-container__news__top-article">
-        <img src="http://source.unsplash.com/random/525x378" alt="" class="main-grid-container__news__top-article__image">
-        <div class="main-grid-container__news__top-article__card-body">
-            <span>Career Advice</span>
-            <h4>Career change advice from computer Turned to a Human Resourcer</h4>
-            <p>
-                <small>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto perspiciatis ut totam illum saepe id blanditiis laboriosam, dolores aut delectus facilis libero tempore molestias dicta nemo assumenda debitis consequuntur ab.    
-                </small>   
-            </p>
-            <a href="#">Read More »</a>
-        </div>
+<article class="main-grid-container__news">
+    <article class="container__news__card">
+         <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__news__card-body">
+                  <h1 class="container__news__card-title">Confluence Course</h1>
+                  <p class="container__news__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__news__card-btn">Get Started »</a>
+            </div>
     </article>
-    <article class="main-grid-container__news__middle-left-article">mid-left</article>
-    <article class="main-grid-container__news__middle-right-article">mid-right</article>
-    <article class="main-grid-container__news__bottom-left-article">bottom-left</article>
-    <article class="main-grid-container__news__bottom-right-article">bottom-right</article>
-</div>
+
+    <article class="container__news__card">
+         <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__news__card-body">
+                  <h1 class="container__news__card-title">Confluence Course</h1>
+                  <p class="container__news__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__news__card-btn">Get Started »</a>
+            </div>
+    </article>
+
+    <article class="container__news__card">
+         <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__news__card-body">
+                  <h1 class="container__news__card-title">Confluence Course</h1>
+                  <p class="container__news__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__news__card-btn">Get Started »</a>
+            </div>
+    </article>
+
+    <article class="container__news__card">
+         <img src="http://source.unsplash.com/random/288x236" alt>
+            <div class="container__news__card-body">
+                  <h1 class="container__news__card-title">Confluence Course</h1>
+                  <p class="container__news__card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  </p>
+                  <a href="#" class="container__news__card-btn">Get Started »</a>
+            </div>
+    </article>
+</article>
