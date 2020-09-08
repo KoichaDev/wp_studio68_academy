da61d6805fa44d07ae398defbd863a08ae5da07a
Khoi Hoang <hello@koicha.dev>
2020-09-08 22:02:36 +0200

    Add/CSS: :sparkles: Added new Grid template area for courses page
    This commit is only for placeholder. Nothing fancy.

--- single-courses.php
+++ single-courses.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
 
+<main class="grid-course-container">
+  <div class="grid-course-container__course-headline">
+      headline
+  </div>
+  <div class="grid-course-container__main">
+      main
+  </div>
+</main>
+
 <?php get_footer(); ?>
