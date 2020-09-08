cf705a1f17d16ecc9a7cc2e04ecfd8611da00e3d
Khoi Hoang <hello@koicha.dev>
2020-09-08 22:07:40 +0200

    Improve Code: :art: Improved folder and file structure for template-parts
    This commit will make sure each page has their own dedicated template parts, so it's easier to organize the code

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 <main class="main-grid-container">
   <?php 
-    get_template_part('template-parts/aside', '/');
-    get_template_part('template-parts/courses_headliner', '/');
-    get_template_part('template-parts/courses', '/');
-    get_template_part('template-parts/news_headliner', '/');
-    get_template_part('template-parts/news', '/');
+    get_template_part('template-parts/home-page/aside', '/');
+    get_template_part('template-parts/home-page/courses_headliner', '/');
+    get_template_part('template-parts/home-page/courses', '/');
+    get_template_part('template-parts/home-page/news_headliner', '/');
+    get_template_part('template-parts/home-page/news', '/');
   ?>
 </main>
 
---
+++ template-parts/home-page/courses_headliner.php
@@ -0,0 +1,6 @@
+<section class="main-grid-container__course__headline">
+    <img src="<?php echo get_theme_file_uri() . './assets/img/df5d4d07b8c380781bf441bd90c67491 1.svg' ?>" alt>
+    <div class="main-grid-container__course__headline__box">
+        <h1>Your Courses</h1>
+    </div>
+</section>
