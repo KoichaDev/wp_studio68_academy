5440060a9ace00ba1d64cb65bf15376da34a69db
Khoi Hoang <hello@koicha.dev>
2020-09-08 18:56:11 +0200

    GUI: :lipstick: Added the featured imaged will be displayed on the front-end
    Every post will be now displayed on the front-end

--- template-parts/news.php
+++ template-parts/news.php
@@ -8,13 +8,14 @@
             $newsArticle -> the_post(); 
       ?>
       <article class="container__news__card">
-            <img src="http://source.unsplash.com/random/288x236" alt>
-                  <div class="container__news__card-body">
-                        <h1 class="container__news__card-title"><?php echo get_the_title(); ?></h1>
-                        <p class="container__news__card-text">
-                              <?php echo wp_trim_words( get_the_excerpt(), 18); ?></p>
-                        <a href="<?php the_permalink(); ?>" class="container__news__card-btn">Get Started »</a>
-                  </div>
+            <img src="<?php the_post_thumbnail_url(); ?>" alt>             
+
+            <div class="container__news__card-body">
+                  <h1 class="container__news__card-title"><?php echo get_the_title(); ?></h1>
+                  <p class="container__news__card-text">
+                        <?php echo wp_trim_words( get_the_excerpt(), 18); ?></p>
+                  <a href="<?php the_permalink(); ?>" class="container__news__card-btn">Get Started »</a>
+            </div>
       </article>
       <?php } 
             wp_reset_postdata();
