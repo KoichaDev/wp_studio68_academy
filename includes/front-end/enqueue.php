c75b0647aef1d54117f4e85b135e5ac2a134397f
Khoi Hoang <hello@koicha.dev>
2020-09-08 18:07:15 +0200

    Remove: :fire: Removed unused JavaScript code
    Removed the moment_js

--- includes/front-end/enqueue.php
+++ includes/front-end/enqueue.php
@@ -1,17 +1,16 @@
 <?php 
     // This is for front-end use. Basically everything related to what the user sees on the webpage
     function s68_front_enqueue_scripts() {
-        
+
         $uri = get_theme_file_uri();
         $check_version = S68_DEV_MODE ? time() : false;
 
         // This will enforcing our WP to get our scripts
         wp_register_style('s68_css_style', $uri . '/assets/css/core.css', [], $check_version);
-        wp_register_script('s68_timezone_js', $uri . '/dist/script.js', [], $check_version);
+        wp_register_script('s68_script_js', $uri . '/dist/script.js', [], $check_version);
  
         // This will load our scripts to work
         wp_enqueue_style('s68_css_style');
-        wp_enqueue_script('s68_moment_js');
-        wp_enqueue_script('s68_timezone_js');
+        wp_enqueue_script('s68_script_js');
     }
 ?>
