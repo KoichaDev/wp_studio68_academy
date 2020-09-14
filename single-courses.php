04ed5b0d2b02c520157d4ed30dfaf13f7ce902ff
Khoi Hoang <hello@koicha.dev>
2020-09-14 21:05:38 +0200

    Add: :sparkles: :lipstick: Added ACF for displaying course
    Displaying:
    - Video Course Preview
    - Course Description
    - Learning Objective

--- single-courses.php
+++ single-courses.php
@@ -1,24 +1,26 @@
 <?php get_header(); ?>
 
-<main class="grid-course-container">
-    <?php 
-        get_template_part('template-parts/courses page/courses_headliner', '/');
-    ?>
-  <section class="grid-course-container__main">
+    <?php  get_template_part('template-parts/courses page/courses_headliner', '/'); ?>
+  <main class="grid-course-container__main">
     <div class="grid-course-container__main__video-preview">
-        <!-- <div class="embed-container">
+        <div class="embed-container">
             <?php the_field('academy_course_video_preview'); ?>
-        </div> -->
+        </div>
     </div>
     <div class="grid-course-container__main__video-description">
         <p>
             <?php the_field('academy_course_description'); ?>
         </p>
+        <br>
+        <br>
         <a href="#" class="btn btn--yellow btn-hover--purple">Get Started »</a>
     </div>
-    <div class="grid-course-container__main__learning-objective">Object</div>
+    <div class="grid-course-container__main__learning-objective">
+        <h1>Learning Objectives</h1>
+        <br>
+        <?php the_field('academy_course_objective_learning'); ?>
+    </div>
     <div class="grid-course-container__main__learning-progress">Progress</div>
-  </section>
-</main>
+  </main>
 
 <?php get_footer(); ?>
