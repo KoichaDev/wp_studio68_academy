8866c62929c53cfa5a8b547b14c5c598dcd22643
Khoi Hoang <hello@koicha.dev>
2020-09-15 01:24:44 +0200

    Update/ChartJs: :lipstick: Updated the ChartJS GUI to look better and smaller
    Next step is to add the legend and description of the Graph

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -24,10 +24,14 @@ export default class ChartJs {
                     borderWidth: 1
                 }]
             },
+
             options: {
-                responsive: true,
-                maintainAspectRatio: true,
-                cutoutPercentage: 70, // Change the doughnut Graph size
+                responsive: false,
+                maintainAspectRatio: false,
+                cutoutPercentage: 80, // Change the doughnut Graph size
+                legend: {
+                    display: false,
+                },
                 scales: {
                     xAxes: [{
                         gridLines: {
@@ -57,14 +61,14 @@ export default class ChartJs {
                     ctx = chart.chart.ctx;
 
                 ctx.restore();
-                const fontSize = (height / 90).toFixed(2);
+                const fontSize = (height / 80).toFixed(2);
                 ctx.font = fontSize + "em sans-serif";
                 ctx.textBaseline = "middle";
                 ctx.fillStyle = "#EED9CA";
 
                 const text = data[0] + "%",
                     textX = Math.round((width - ctx.measureText(text).width) / 2 + 12),
-                    textY = height / 2 + 13;
+                    textY = height / 2;
 
                 ctx.fillText(text, textX, textY);
                 ctx.save();
--- dist/script.js
+++ dist/script.js
@@ -5887,10 +5887,13 @@ var ChartJs = /*#__PURE__*/function () {
           }]
         },
         options: {
-          responsive: true,
-          maintainAspectRatio: true,
-          cutoutPercentage: 70,
+          responsive: false,
+          maintainAspectRatio: false,
+          cutoutPercentage: 80,
           // Change the doughnut Graph size
+          legend: {
+            display: false
+          },
           scales: {
             xAxes: [{
               gridLines: {
@@ -5919,13 +5922,13 @@ var ChartJs = /*#__PURE__*/function () {
               height = chart.chart.height,
               ctx = chart.chart.ctx;
           ctx.restore();
-          var fontSize = (height / 90).toFixed(2);
+          var fontSize = (height / 80).toFixed(2);
           ctx.font = fontSize + "em sans-serif";
           ctx.textBaseline = "middle";
           ctx.fillStyle = "#EED9CA";
           var text = data[0] + "%",
               textX = Math.round((width - ctx.measureText(text).width) / 2 + 12),
-              textY = height / 2 + 13;
+              textY = height / 2;
           ctx.fillText(text, textX, textY);
           ctx.save();
         }
@@ -5978,7 +5981,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60965" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64495" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
