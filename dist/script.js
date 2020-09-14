7483f64fef98aa47d90785f52db8bcd003dbcbc0
Khoi Hoang <hello@koicha.dev>
2020-09-15 00:37:19 +0200

    Update/Chartjs: :sparkles: Remove ChartJs Background Grid Lines

--- dist/script.js
+++ dist/script.js
@@ -5874,20 +5874,30 @@ var ChartJs = /*#__PURE__*/function () {
     key: "event",
     value: function event() {
       new Chart(this.ctx, {
-        type: 'bar',
+        type: 'doughnut',
         data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          labels: ['Complete', 'Incomplete'],
           datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-            borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+            data: [60, 40],
+            backgroundColor: ['rgba(204, 71, 115, 1)', 'rgba(71, 204, 160, 1)'],
+            borderColor: ['rgba(204, 71, 115, 1)', 'rgba(71, 204, 160, 1)'],
             borderWidth: 1
           }]
         },
         options: {
           scales: {
+            xAxes: [{
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                beginAtZero: true
+              }
+            }],
             yAxes: [{
+              gridLines: {
+                display: false
+              },
               ticks: {
                 beginAtZero: true
               }
