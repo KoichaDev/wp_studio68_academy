ea681c9a90ddcc3005809f888770c1c33db348fa
Khoi Hoang <hello@koicha.dev>
2020-09-14 20:26:32 +0200

    Add/CSS/btn: :sparkles: Added button feature for CSS component
    - Add button default styling 
    - Added button yellow as Block Element modifier
    - Added Button hover purple as Block Element modifier

--- single-courses.php
+++ single-courses.php
@@ -4,14 +4,21 @@
     <?php 
         get_template_part('template-parts/courses page/courses_headliner', '/');
     ?>
-  <div class="grid-course-container__main">
-        <div class="grid-course-intro">
-            <div class="grid-course-intro__video-preview">video</div>
-            <div class="grid-course-intro__video-description">Description</div>
-            <div class="grid-course-intro__learning-objective">Object</div>
-            <div class="grid-course-intro__learning-progress">Progress</div>
-        </div>
-  </div>
+  <section class="grid-course-container__main">
+    <div class="grid-course-container__main__video-preview">
+        <!-- <div class="embed-container">
+            <?php the_field('academy_course_video_preview'); ?>
+        </div> -->
+    </div>
+    <div class="grid-course-container__main__video-description">
+        <p>
+            <?php the_field('academy_course_description'); ?>
+        </p>
+        <a href="#" class="btn btn--yellow btn-hover--purple">Get Started »</a>
+    </div>
+    <div class="grid-course-container__main__learning-objective">Object</div>
+    <div class="grid-course-container__main__learning-progress">Progress</div>
+  </section>
 </main>
 
 <?php get_footer(); ?>
