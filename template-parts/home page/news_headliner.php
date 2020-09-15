7bf4ec110a289804ad80b0b10eb8a2278be1eefb
Khoi Hoang <hello@koicha.dev>
2020-09-15 21:27:28 +0200

    Update/GUI: :lipstick: Updated News & Noteworthy font size and added svg image
    - The font size should be responsive
    - Added new svg image to look nice

--- template-parts/home page/news_headliner.php
+++ template-parts/home page/news_headliner.php
@@ -1,5 +1,6 @@
 <section class="main-grid-container__news_headliner">
     <img src="<?php echo get_theme_file_uri() . './assets/img/polygon.svg' ?>" alt>
+    <img src="<?php echo get_theme_file_uri() . './assets/img/73855ee6a33c3e6c7c78c02e1d399a85 1.svg' ?>" alt>
     <div class="main-grid-container__news_headliner__box">
         <h1>News & Noteworthy</h1>
     </div>
