e517ffc85a1d005312a4107febf78f2c8087b558
Khoi Hoang <hello@koicha.dev>
2020-09-16 08:39:08 +0200

    Update/CSS: :lipstick: Partially fixed the layout of the course section
    - Skills Covered section
    - Learning Objectives

--- dist/script.js
+++ dist/script.js
@@ -6053,7 +6053,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60351" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57680" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
