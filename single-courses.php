cc67a7ca60e4cef1b2b654565d31a331fcbd9c98
Khoi Hoang <hello@koicha.dev>
2020-09-18 01:50:33 +0200

    Update/php: :sparkles: Updated the logic of wordpress to direct the website of specific course
    When user wants to click one on a specific course, then it will direct that user to that course to watch. Currently the php file that direct to the user is placeholder text

--- single-courses.php
+++ single-courses.php
@@ -2,11 +2,23 @@
 // This is the entry point of WP Custom Post Type. Usually, this is only dedicated single-post type,
 // Since WP doesn't have in-built parent-child relationship, so we can create this logic instead
 
-if (isset($_GET["section"]) && $_GET["section"] == "preview") {
-    // Direct access to the preview of the course
-    require_once "academy-course-preview.php";
-} else {
-    // Direct access to the course description
-    require_once "academy-course.php";
+$rows = get_field('academy_course_content');
+
+if($rows) {
+    foreach( $rows as $row ) {
+        
+        if(isset($_GET["section"]) || $_GET["section"] == $row["academy_course_title"]) {
+            require_once "academy-course-specific-preview.php";
+        } else if (isset($_GET["section"]) && $_GET["section"] == "preview") {
+            // Direct access to the preview of the course
+            require_once "academy-course-preview.php";
+
+        } else {
+            // Direct access to the course description
+            require_once "academy-course.php";
+        }
+    }
 }
+
+
 
