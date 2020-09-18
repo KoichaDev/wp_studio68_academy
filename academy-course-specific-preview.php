fa18555dda3ba20527e4b9d612b1e3b9a0ff749a
Khoi Hoang <hello@koicha.dev>
2020-09-18 02:12:57 +0200

    Add/lipstick: :lipstick: ✅ Added an embedded youtube video to check if it's working
    It's to check if the embedded video is working when user is clicking on a specific course

--- academy-course-specific-preview.php
+++ academy-course-specific-preview.php
@@ -3,9 +3,8 @@ $rows = get_field('academy_course_content');
 if( $rows ) {
     foreach( $rows as $row ) {
         $permalink = get_the_permalink() . '?section=' . $row['academy_course_title'];  
-
         if($permalink && isset($_GET["section"]) && $_GET["section"] == $row['academy_course_title']) {
-            echo $row['academy_course_title'];
+            echo $row['academy_course_embedded'];
         } 
     }
 }
