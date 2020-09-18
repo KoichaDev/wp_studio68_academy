f867d127ce585070779c349c384da85448ceb931
Khoi Hoang <hello@koicha.dev>
2020-09-18 02:08:52 +0200

    Add/test: :✅: added logic test on the specific course on php to check if it's correct url
    If it's correct url, then we want to do something. In this case, we can print our the course title, course image whatever. It's just for testing purpose

--- academy-course-specific-preview.php
+++ academy-course-specific-preview.php
@@ -1,1 +1,12 @@
-preview biiii
+<?php 
+$rows = get_field('academy_course_content');
+if( $rows ) {
+    foreach( $rows as $row ) {
+        $permalink = get_the_permalink() . '?section=' . $row['academy_course_title'];  
+
+        if($permalink && isset($_GET["section"]) && $_GET["section"] == $row['academy_course_title']) {
+            echo $row['academy_course_title'];
+        } 
+    }
+}
+?>
