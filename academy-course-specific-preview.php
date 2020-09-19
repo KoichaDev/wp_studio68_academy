901248864128094a5afa5c6b3a181d4c2e04e3e5
Khoi Hoang <hello@koicha.dev>
2020-09-19 02:41:08 +0200

    Add/GUI: :lipstick: Updated the specific course preview for list of the courses that exist on sidebar
    Currently, the list is displayed, but it's not functional working intended as we would like it to be. Next step is fixing the design of the course list

--- academy-course-specific-preview.php
+++ academy-course-specific-preview.php
@@ -1,11 +1,95 @@
-<?php 
-$rows = get_field('academy_course_content');
-if( $rows ) {
-    foreach( $rows as $row ) {
-        $permalink = get_the_permalink() . '?section=' . $row['academy_course_title'];  
-        if($permalink && isset($_GET["section"]) && $_GET["section"] == $row['academy_course_title']) {
-            echo $row['academy_course_embedded'];
-        } 
-    }
-}
-?>
+<?php get_header(); ?>
+
+
+<main class="preview-grid-container">
+  <div class="preview-grid__courses">
+     <?php 
+            $rows = get_field('academy_course_content');
+            if( $rows ) {
+                echo '<ul>';
+                foreach( $rows as $row ) {
+                    if($row['academy_course_section'] !== '') { 
+                            ?>
+                            <h2><?php echo $row['academy_course_section']; ?> </h2>
+                            <hr>
+                            <li>
+                                <a href="<?php echo get_the_permalink() . '?section=' . $row['academy_course_title']; ?>"> 
+                        <?php if($row['academy_course_section'] !== '') { ?>
+                            <div class="table-of-content-container__information" data-duration>
+                                <h3>
+                                    <?php echo $row['academy_course_title']; ?>
+                                    <small>(<?php echo $row['academy_course_duration']; ?> min) </small>
+                                </h3>
+                            </div>
+                            </a>
+                        </li>
+                        <?php
+                    } else {
+                         ?>
+                        <li>
+                            <a href="<?php echo get_the_permalink() . '?section=' . $row['academy_course_title']; ?>"> 
+                            <h3>
+                                <?php echo $row['academy_course_title']; ?>
+                                <small>(<?php echo $row['academy_course_duration']; ?> min) </small>
+                            </h3>
+                        </li>
+                        </a>
+                        <?php
+                    }
+                    ?>
+                            <?php
+                        } else {
+                              ?>
+                              <li>
+                                <a href="<?php echo get_the_permalink() . '?section=' . $row['academy_course_title']; ?>"> 
+                               
+                            <?php if($row['academy_course_section'] == '') { ?>
+                            <div class="table-of-content-container__information" data-duration>
+                                <h3>
+                                    <?php echo $row['academy_course_title']; ?>
+                                    <small>(<?php echo $row['academy_course_duration']; ?> min) </small>
+                                </h3>
+                            </div>
+                            </a>
+                        </li>
+                        <?php
+                    } else {
+                         ?>
+                        <li>
+                            <a href="<?php echo get_the_permalink() . '?section=' . $row['academy_course_title']; ?>"> 
+                            <h3>
+                                <?php echo $row['academy_course_title']; ?>
+                                <small>(<?php echo $row['academy_course_duration']; ?> min) </small>
+                            </h3>
+                        </li>
+                        </a>
+                        <?php
+                    }
+                    ?>
+                            </li>
+                            <?php
+                        }
+                }
+                echo '</ul>';
+                ?>
+            </div>;<!-- End Div grid -->
+            <?php
+            }
+        ?>  
+  </div>
+  <div class="preview-grid__video-preview">
+    <?php 
+        $rows = get_field('academy_course_content');
+        if( $rows ) {
+            foreach( $rows as $row ) {
+                $permalink = get_the_permalink() . '?section=' . $row['academy_course_title'];  
+                if($permalink && isset($_GET["section"]) && $_GET["section"] == $row['academy_course_title']) {
+                    echo $row['academy_course_embedded'];
+                } 
+            }
+        }
+    ?>
+  </div>
+</main>
+
+<?php get_footer(); ?>
