a3f55f7dfe1155f034a4c8dda0193264dc63eefa
Khoi Hoang <hello@koicha.dev>
2020-09-21 19:24:56 +0200

    Update/API: :sparkles: Updated the Customized REST API for search specific post ID

--- includes/back-end/search-route.php
+++ includes/back-end/search-route.php
@@ -3,24 +3,26 @@
 add_action('rest_api_init', 's68_course_api');
 
 function s68_course_api() {
-    register_rest_route('s68/v1', 'course', [
-        'methods' => WP_REST_SERVER::READABLE,
+    register_rest_route('s68/v1', 'course/(?P<id>\d+)', [
+        'methods' => WP_REST_SERVER::ALLMETHODS ,
         'callback' => 's68_course'
     ]);
 }
 
 function s68_course($data) {
+
+  
     $course = new WP_Query([
         'post_type' => 'courses',
-        's' => sanitize_text_field($data['q'])
+        'p' => sanitize_text_field($data['id'])
     ]);
 
     $course_api = [];
 
-
     while($course -> have_posts()) {
         $course -> the_post();
         array_push($course_api, [
+            'id' => get_the_ID(),
             'title' => get_the_title(),
             'permalink' => get_the_permalink(),
             'course' => [ 
